9fc254445fea912b32802a82bf83d38c198cf2e6
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-03-21 16:38:49 +0800

    Add files via upload
    - Added scoring
    - Added snake increasing in length when eating apples
    - Added apple moving to random location

--- Snake Game.py
+++ Snake Game.py
@@ -1,18 +1,43 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
-class Snake:
+SIZE = 20
+
+class Apple:
     def __init__(self, screen):
         self.screen = screen
-        self.snake = pygame.image.load('snake.png').convert()
-        self.x_coord = 150
-        self.y_coord = 150
-        self.direction = 'up'
+        self.apple = pygame.image.load('apple.png').convert()
+        self.x_coord = (random.randint(2, 38) * SIZE) + 10
+        self.y_coord = (random.randint(2, 28) * SIZE) + 10
 
     def draw(self):
+        self.screen.blit(self.apple, (self.x_coord, self.y_coord))
+        pygame.display.flip()
+
+    def move(self):
+        self.x_coord = (random.randint(2, 38) * SIZE) + 10
+        self.y_coord = (random.randint(2, 28) * SIZE) + 10
+
+class Snake:
+    def __init__(self, screen, length, x_start, y_start):
+        self.length = length
+        self.screen = screen
+        self.snake = pygame.image.load('snake.png').convert()
+        self.x_coord = [x_start] * length
+        self.y_coord = [y_start] * length
+        self.direction = 'up'
+
+    def increase_length(self):
+        self.length += 1
+        self.x_coord.append(-1)
+        self.y_coord.append(-1)
+    
+    def draw(self):
         self.screen.fill((255, 255, 255))
-        self.screen.blit(self.snake, (self.x_coord, self.y_coord))
+        for i in range(self.length):
+            self.screen.blit(self.snake, (self.x_coord[i], self.y_coord[i]))
         pygame.display.flip()
 
     def up(self):
@@ -28,27 +53,55 @@ class Snake:
         self.direction = 'right'
 
     def move(self):
+        for i in range(self.length - 1 , 0, -1):
+            self.x_coord[i] = self.x_coord[i - 1]
+            self.y_coord[i] = self.y_coord[i - 1]
+        
         if self.direction == 'up':
-            self.y_coord -= 10
+            self.y_coord[0] -= SIZE
         if self.direction == 'down':
-            self.y_coord += 10
+            self.y_coord[0] += SIZE
         if self.direction == 'left':
-            self.x_coord -= 10
+            self.x_coord[0] -= SIZE
         if self.direction == 'right':
-            self.x_coord += 10
+            self.x_coord[0] += SIZE
         
         self.draw()
-
 
 class Game:
     def __init__(self):
         # initialise the board
         pygame.init()
-        self.board = pygame.display.set_mode((500, 500))
+        self.board = pygame.display.set_mode((800, 600))
         # change colour of the background
         self.board.fill((255, 255, 255))
-        self.snake = Snake(self.board)
+        
+        self.snake = Snake(self.board, 3, 150, 150)
         self.snake.draw()
+        self.apple = Apple(self.board)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.move()
+        self.apple.draw()
+        self.score()
+        pygame.display.flip()
+
+        if self.eat_apple(self.snake.x_coord[0], self.snake.y_coord[0], self.apple.x_coord, self.apple.y_coord):
+            self.apple.move()
+            self.snake.increase_length()
+    
+    def eat_apple(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+        return False
+
+    def score(self):
+        font = pygame.font.SysFont('arial', 20)
+        score = font.render('Score: ' + str(self.snake.length), True, (0, 0, 0))
+        self.board.blit(score, (650, 15))
 
     def run(self):
         # open the window until it is closed
@@ -71,14 +124,12 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.move()
-            time.sleep(0.25)
+            self.play()
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
     game.run()
 
-
-    #apple = pygame.image.load('apple.png').convert()
 
 
